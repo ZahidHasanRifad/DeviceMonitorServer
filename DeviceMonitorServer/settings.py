23e1d90b82e1fb5ea26dd553fd5eadd192040222
Zahid Hasan Rifad <zrifad815@gmail.com>
2021-03-03 20:16:39 +0600

    key remove to text file

--- DeviceMonitorServer/settings.py
+++ DeviceMonitorServer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nrm(ql)ohj++lo#kv2&$u27o$qg5uidc81ix$n%yk9s$-ub9*c'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
